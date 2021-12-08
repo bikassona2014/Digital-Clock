f44b732c556f7a4e170b0313a40e1cdd5e0af4de
Your Name <bkm20071999@gmail.com>
2021-12-08 12:21:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const App = () => {
+    let time = new Date().toLocaleTimeString();
+    const[ctime, setCtime] = useState(time);
+    
+    const updateTime = ()=>{
+        time = new Date().toLocaleTimeString();
+        setCtime(time);
+    };
+
+    setInterval(updateTime, 1000);
+
+    return (
+        <>
+        <div>
+        <h1> {ctime} </h1>
+        </div>
+        </>
+    );
+};
+
+export default App;
+
